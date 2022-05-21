600f0d7a7798d3cc8786f8482fda7222f0c17a7f
Javier Molina <javiermolinalorente@gmail.com>
2022-05-21 18:35:20 +0200

    fix: drawer direction rtl

--- app/src/main/java/es/javier/cappcake/presentation/profilescreen/ProfileScreen.kt
+++ app/src/main/java/es/javier/cappcake/presentation/profilescreen/ProfileScreen.kt
@@ -14,12 +14,15 @@ import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Surface
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavGraph.Companion.findStartDestination
@@ -38,69 +41,71 @@ fun ProfileScreen(navController: NavController, viewModel: ProfileScreenVIewMode
         viewModel.loadRecipes(uid = uid)
     }
 
-    Scaffold(
-        drawerContent = {
-            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                Button(onClick = { signOut(navController = navController) }) {
-                    Text(text = "Cerrar sesión".uppercase())
-                }
-            }
-        }
-    ) {
-        Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Box {
-                Column(modifier = Modifier.padding(top = 10.dp), horizontalAlignment = Alignment.CenterHorizontally) {
-                    ProfileScreenProfileImage(
-                        modifier = Modifier.size(90.dp),
-                        profileImage = viewModel.user?.profileImage)
-
-                    if (viewModel.user == null) {
-                        Text(text = "Username", modifier = Modifier.padding(vertical = 10.dp))
-                    } else {
-                        Text(text = viewModel.user!!.username, modifier = Modifier.padding(vertical = 10.dp))
-                    }
-
-                    Row(modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(bottom = 10.dp), verticalAlignment = Alignment.CenterVertically){
-                        Column(modifier = Modifier.weight(1f), horizontalAlignment = Alignment.CenterHorizontally) {
-                            Text(text = "Post")
-                            Text(text = "1")
-                        }
-
-                        Column(modifier = Modifier.weight(1f), horizontalAlignment = Alignment.CenterHorizontally) {
-                            Text(text = "Following")
-                            Text(text = "1")
-                        }
-
-                        Column(modifier = Modifier.weight(1f), horizontalAlignment = Alignment.CenterHorizontally) {
-                            Text(text = "Followers")
-                            Text(text = "1")
-                        }
-                    }
-
-                    Divider()
-                }
-            }
-
-            if (viewModel.recipes != null) {
-                LazyColumn(modifier = Modifier
-                    .fillMaxWidth()
-                    .weight(1f)) {
-
-                    items(viewModel.recipes!!, key = { it.recipeId }) {
-
-                        RecipeComponent(
-                            modifier = Modifier.padding(20.dp),
-                            recipe = it,
-                            loadUser = { viewModel.user },
-                            onRecipeClick = { navController.navigate(Navigation.RecipeDetailScreen.navigationRoute + "?recipeId=${it.recipeId}") }
-                        )
-                    }
-                }
-            } else {
+    CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
+        Scaffold(
+            drawerContent = {
                 Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                    CircularProgressIndicator()
+                    Button(onClick = { signOut(navController = navController) }) {
+                        Text(text = "Cerrar sesión".uppercase())
+                    }
+                }
+            }
+        ) {
+            Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+                Box {
+                    Column(modifier = Modifier.padding(top = 10.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+                        ProfileScreenProfileImage(
+                            modifier = Modifier.size(90.dp),
+                            profileImage = viewModel.user?.profileImage)
+
+                        if (viewModel.user == null) {
+                            Text(text = "Username", modifier = Modifier.padding(vertical = 10.dp))
+                        } else {
+                            Text(text = viewModel.user!!.username, modifier = Modifier.padding(vertical = 10.dp))
+                        }
+
+                        Row(modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 10.dp), verticalAlignment = Alignment.CenterVertically){
+                            Column(modifier = Modifier.weight(1f), horizontalAlignment = Alignment.CenterHorizontally) {
+                                Text(text = "Post")
+                                Text(text = "1")
+                            }
+
+                            Column(modifier = Modifier.weight(1f), horizontalAlignment = Alignment.CenterHorizontally) {
+                                Text(text = "Following")
+                                Text(text = "1")
+                            }
+
+                            Column(modifier = Modifier.weight(1f), horizontalAlignment = Alignment.CenterHorizontally) {
+                                Text(text = "Followers")
+                                Text(text = "1")
+                            }
+                        }
+
+                        Divider()
+                    }
+                }
+
+                if (viewModel.recipes != null) {
+                    LazyColumn(modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f)) {
+
+                        items(viewModel.recipes!!, key = { it.recipeId }) {
+
+                            RecipeComponent(
+                                modifier = Modifier.padding(20.dp),
+                                recipe = it,
+                                loadUser = { viewModel.user },
+                                onRecipeClick = { navController.navigate(Navigation.RecipeDetailScreen.navigationRoute + "?recipeId=${it.recipeId}") }
+                            )
+                        }
+                    }
+                } else {
+                    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                        CircularProgressIndicator()
+                    }
                 }
             }
         }
